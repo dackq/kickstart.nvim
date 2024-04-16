636eccf8fede1d382da3c4461a56b60d2d4e92a8
Dakote Reed <dakotalreed@gmail.com>
2024-04-16 21:43:21 +0200

    activate some language servers

--- init.lua
+++ init.lua
@@ -540,8 +540,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
+        java_language_server = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

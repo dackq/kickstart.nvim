6068bb1c9b1ad2763bae2bdd6217c38f3dfefff6
Dakote Reed <dakotalreed@gmail.com>
2024-04-19 18:47:09 +0200

    add nvim-tree

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,24 @@
+-- config for https://github.com/nvim-tree/nvim-tree.lua
+
+local function on_attach_buffer(buffer_number)
+  local api = require 'nvim-tree.api'
+  -- default mappings
+  api.config.mappings.default_on_attach(buffer_number)
+end
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  init = function()
+    -- disable netrw
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+  config = function()
+    require('nvim-tree').setup {
+      on_attach = on_attach_buffer,
+    }
+    local api = require 'nvim-tree.api'
+    -- custom mappings
+    vim.keymap.set('n', '<leader>ve', api.tree.toggle, { desc = '[V]iew: toggle nvim-tree [E]xplorer', noremap = true, silent = true, nowait = true })
+  end,
+}

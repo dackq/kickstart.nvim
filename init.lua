383e9249dc1a58e5913e361ffc67e30713bbd137
Dakota Lee Reed <dakota.reed@iongroup.com>
2024-05-03 09:05:42 +0200

    remove language servers

--- init.lua
+++ init.lua
@@ -542,9 +542,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-        java_language_server = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- java_language_server = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

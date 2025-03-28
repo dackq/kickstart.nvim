37fac9b5ff3294ebf5c4fce7327f236669d17c2f
Dakota Lee Reed <dakota.reed@iongroup.com>
2025-03-28 16:41:54 +0100

    remove java-language-server

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -140,7 +140,6 @@ return { -- LSP Configuration & Plugins
       yamlls = {},
       bashls = {},
       rust_analyzer = {},
-      java_language_server = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:

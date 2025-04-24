669155dcfd86c9bf103ecb7a285f1264849496ff
Dakota Lee Reed <dakota.reed@iongroup.com>
2025-04-24 18:11:53 +0200

    feat: update scrolloff

--- init.lua
+++ init.lua
@@ -158,7 +158,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 1000
 
 -- Fold
 -- vim.opt.foldmethod = 'syntax'
